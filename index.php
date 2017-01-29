2f04f7e955a26212cebcb6e7b9caa922475ffa06
Kevin deLeon <kevin.deleon@gmail.com>
2017-01-29 12:58:51 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Longshore | Shopify Development &amp; Digital Marketing Agency</title>
+	<meta name="description" content="Longshore is a digital marketing agency specializing in ecommerce, Shopify and WooCommerce development. We use data and experience to drive your success.">
+	<link rel="shortcut icon" href="/favicon.ico">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta property="og:title" content="Longshore | Shopify Development &amp; Digital Marketing Agency">
+	<meta property="og:description" content="Longshore is a digital marketing agency specializing in ecommerce, Shopify and WooCommerce development. We use data and experience to drive your success.">
+	<meta property="og:image" content="//longshore.agency/assets/images/web-blue.png">
+	<link rel="canonical" href="https://longshore.agency">
+	<link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/assets/css/font-awesome.min.css">
+	<link rel="stylesheet" href="/assets/css/site.css">
+</head>
+<body>
+	<section class="content center">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="logo-area">
+					<img class="logo" src="/assets/images/web-white.png" alt="Longshore Logo">
+				</div>
+			</div>
+		</div>
+	</section>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
+</body>
+</html>
