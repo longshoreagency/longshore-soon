905797bc95c80504579bba1dd2963ce3aa7bd216
Kevin deLeon <kevin.deleon@gmail.com>
2017-01-29 17:47:51 -0600

    adds utilities and tracking

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="google-site-verification" content="6YLDJwRNPJKHqPS_B9xvP6aIYFMagn7NNBoDbh-YV-A" />
 	<title>Longshore | Shopify Development &amp; Digital Marketing Agency</title>
 	<meta name="description" content="Longshore is a digital marketing agency specializing in ecommerce, Shopify and WooCommerce development. We use data and experience to drive your success.">
 	<link rel="shortcut icon" href="/favicon.ico">
@@ -10,10 +11,97 @@
 	<meta property="og:title" content="Longshore | Shopify Development &amp; Digital Marketing Agency">
 	<meta property="og:description" content="Longshore is a digital marketing agency specializing in ecommerce, Shopify and WooCommerce development. We use data and experience to drive your success.">
 	<meta property="og:image" content="//longshore.agency/assets/images/web-blue.png">
+	<script type="text/javascript" src="/assets/js/ls-utils.js"></script>
 	<link rel="canonical" href="https://longshore.agency">
 	<link rel="stylesheet" href="/assets/css/bootstrap.min.css">
 	<link rel="stylesheet" href="/assets/css/font-awesome.min.css">
 	<link rel="stylesheet" href="/assets/css/site.css">
+
+	<!-- Google Analytics -->
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		var utm_params, key, referrer, utm, uuid, _ref, gacookie; 
+		
+		function uuid() { 
+			return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) { 
+				var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8); 
+				return v.toString(16); 
+			}); 
+		}
+
+		if (!LSUtils.readCookie("user_id")) { 
+			LSUtils.createCookie("user_id", uuid(), 180); 
+		}
+
+		if (!LSUtils.readCookie("utm_params")) {
+			utm = {
+				utm_campaign: "-",
+				utm_content: "-",
+				utm_medium: "-",
+				utm_source: "direct",
+				utm_term: "-"
+			};
+
+			if (document.location.search.indexOf('utm_source=') > -1 || document.location.search.indexOf('utm_medium=') >-1 || document.location.search.indexOf('utm_campaign=') >-1){
+				locationString = LSUtils.parseURL(document.location);
+				for (key in utm) {
+				if (typeof locationString.searchObject[key] !== "undefined") {
+					utm[key] = locationString.searchObject[key];
+					}
+				}
+			}
+			else {
+				referrer = LSUtils.parseURL(document.referrer);
+				_ref = ['google', 'bing', 'yahoo'];
+				for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+					searchengine = _ref[_i];
+					if (referrer.hostname.indexOf("." + searchengine + ".") !== -1) {
+						utm.utm_source = searchengine;
+						utm.utm_medium = "organic";
+						break;
+					}
+				}
+				if (utm.utm_medium !== "organic") {
+					if (document.referrer.length > 0)
+						utm.utm_campaign = document.referrer;
+					else
+						utm.utm_campaign = "-";
+				}
+			}
+
+			utm_params = [];
+			for (key in utm) {
+				val = utm[key]
+				utm_params.push(encodeURIComponent(key) + '=' + encodeURIComponent(val))
+			}
+
+			utm_params = utm_params.join('&');
+			LSUtils.createCookie("utm_params", utm_params, 180);
+		}
+
+		ga('create', 'UA-91089429-1', 'auto', {'allowLinker': true, 'siteSpeedSampleRate': 100});
+		ga('require', 'displayfeatures');
+		ga('require', 'linker');
+		ga('set', 'dimension1', LSUtils.readCookie("user_id"));
+		ga('set', 'dimension2', (new Date()).getTime().toString());
+		ga('send', 'pageview');
+
+		// Get a  Google Analytics cookie id value to pass on hubspot form submission (gAnalyticsCookieID hidden field)
+		ga(function(t) { 
+			gacookie = t.get('clientId'); 
+		});
+
+		// Google Anayltics Error Handling
+		window.onerror = function(message, url, linenumber, colnumber) {  
+			var ga = window.ga || function(){  };
+			ga('send', 'event', 'JS Error', message, navigator.userAgent + ' -> ' + url + " @ line: " + linenumber + ", col: " + colnumber, {  'nonInteraction': 1});
+			return false; 
+		}
+	</script>
 </head>
 <body>
 	<section class="content">
